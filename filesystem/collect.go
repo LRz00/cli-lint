7324f52d4951e7d285de188871b76400a794b1d3
Lara Rodrigues <lararods73@gmail.com>
2026-02-04 11:02:08 -0300

    add file collector by flag language and path

---
+++ filesystem/collect.go
@@ -0,0 +1,49 @@
+package filesystem
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+const (
+	javascript = "javascript"
+	python     = "python"
+)
+
+var extensions = map[string]string{
+	javascript: ".js",
+	python:     ".py",
+}
+
+func CollectFiles(root string, lang string) ([]string, error) {
+	var files []string
+
+	// WalkDir walks the file tree rooted at root, calling fn for each file or directory in the tree, including root.
+	err := filepath.WalkDir(root, func(path string, d os.DirEntry, err error) error {
+
+		ext, ok := extensions[lang]
+		if !ok {
+			return fmt.Errorf("unsupported language: %s", lang)
+		}
+
+		// If there is an error accessing the path, return the error.
+		if err != nil {
+			return err
+		}
+
+		// If the entry is a directory, skip it.
+		if d.IsDir() {
+			return nil
+		}
+
+		// If the file has the lang extension, add it to the list.
+		if filepath.Ext(path) == ext {
+			files = append(files, path)
+		}
+
+		return nil
+	})
+
+	return files, err
+}
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+
+	"github.com/LRz00/cli-lint/filesystem"
 )
 
 //os.args[0] = program name
@@ -13,6 +15,14 @@ func main() {
 	lang := flag.String("lang", "", "Programming language of the CLI application (python or javascript)")
 
 	flag.Parse()
+
+	files, err := filesystem.CollectFiles(*path, *lang)
+	if err != nil {
+		fmt.Printf("Error collecting files: %v\n", err)
+		return
+	}
+
+	fmt.Printf("%s files found: %d\n", *lang, len(files))
 
 	fmt.Printf("path: %s\n", *path)
 	fmt.Printf("lang: %s\n", *lang)
